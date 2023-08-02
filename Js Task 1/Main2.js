2297bdf53f1bef6c95c27438e579a43a907ef23a
samer <sameradel789@gmail.com>
2023-08-03 00:18:12 +0300

    make descriptive function names

--- Js Task 1/Main2.js
+++ Js Task 1/Main2.js
@@ -24,248 +24,251 @@ function isVowel(character) {
         return false
     }
 }
+function getGrade(mark) {
+    var percentage = (mark / 100) * 100; // Assuming full mark is 100
+    if (percentage >= 90) {
+        return "Grade A";
+    } else if (percentage >= 80) {
+        return "Grade B";
+    } else if (percentage >= 70) {
+        return "Grade C";
+    } else if (percentage >= 60) {
+        return "Grade D";
+    } else if (percentage >= 40) {
+        return "Grade E";
+    } else {
+        return "Grade F";
+    }
+}
 
-// function question1() {
-//     var num = GetInput("a number");
-//     displayResult(num);
-// }
+//Question 1
+function printNumber() {
+    var num = GetInput("a number");
+    displayResult(num);
+}
 
-// function question2() {
-//     var num = GetInput("a number");
-//     if (num % 3 == 0 && num % 4 == 0) {
-//         displayResult("Yes");
-//     } else {
-//         displayResult("No");
-//     }
-// }
+//Question 2
+function divisable() {
+    var num = GetInput("a number");
+    if (num % 3 == 0 && num % 4 == 0) {
+        displayResult("Yes");
+    } else {
+        displayResult("No");
+    }
+}
 
-// function question3() {
-//     var num1 = GetInput("Num 1");
-//     var num2 = GetInput("Num 2");
-//     var max = Math.max(num1, num2);
-//     displayResult(max);
-// }
+//Question 3
+function maxNum() {
+    var num1 = GetInput("Num 1");
+    var num2 = GetInput("Num 2");
+    var max = Math.max(num1, num2);
+    displayResult(max);
+}
 
-// function question4() {
-//     var num1 = GetInput("a number");
-//     if (num1 < 0) {
-//         displayResult("negative");
-//     } else {
-//         displayResult("positive");
-//     }
-// }
+//Question 4
+function iSNegative() {
+    var num1 = GetInput("a number");
+    if (num1 < 0) {
+        displayResult("negative");
+    } else {
+        displayResult("positive");
+    }
+}
 
-// function question5() {
-//     var num1 = GetInput("number 1");
-//     var num2 = GetInput("number 2");
-//     var num3 = GetInput("number 3");
-//     var maxNumber = Math.max(num1, num2, num3);
-//     var minNumber = Math.min(num1, num2, num3);
-//     displayResult("Max number is " + maxNumber);
-//     document.getElementById("Second_line").innerHTML = "Min number is " + minNumber;
-// }
+//Question 5
+function maxAndMin() {
+    var num1 = GetInput("number 1");
+    var num2 = GetInput("number 2");
+    var num3 = GetInput("number 3");
+    var maxNumber = Math.max(num1, num2, num3);
+    var minNumber = Math.min(num1, num2, num3);
+    displayResult("Max number is " + maxNumber);
+    document.getElementById("Second_line").innerHTML = "Min number is " + minNumber;
+}
 
-// function question6&7() {
-//     var num1 = GetInput("a number");
-//     if (num1 % 2 == 0) {
-//         displayResult("even");
-//     } else {
-//         displayResult("odd");
-//     }
-// }
+// Question 6&7
+function evenOrOdd() {
+    var num1 = GetInput("a number");
+    if (num1 % 2 == 0) {
+        displayResult("even");
+    } else {
+        displayResult("odd");
+    }
+}
+
+//Question 8
+function vowelChar() {
+    var character = GetInput("a character");
+    if (isVowel(character)) {
+        displayInAlert("Vowel")
+    } else {
+        displayInAlert("Consonant");
+    }
+}
+
+//Question 9
+function printAllNumbers() {
+    var number = GetInput("a number");
+    var numbersPrinted = "";
+    for (let i = 1; i <= number; i++) {
+        numbersPrinted += i + " ";
+    }
+    displayResult(numbersPrinted)
+}
+
+//Question 10
+function multiplicationTable() {
+    var number = GetInput("a number");
+    let numbersPrinted = "";
+    for (let i = 1; i <= 12; i++) {
+        numbersPrinted += number * i + " ";
+    }
+    displayResult(numbersPrinted);
+}
+
+//Question 11
+function printEvenNumbers() {
+    var number = GetInput("a number");
+    let numbersPrinted = "";
+    for (let i = 1; i <= number; i++) {
+        if (i % 2 == 0) {
+            numbersPrinted += i + " ";
+        }
+    }
+    displayResult(numbersPrinted);
+}
+
+//Question 12 -1
+function power() {
+    var number = GetInput("a number");
+    var power = GetInput("the power");
+    displayInAlert(Math.pow(number, power));
+}
+
+//Question 12 -2
+
+function calcStatisticss() {
+    var sum = 0;
+    for (let i = 1; i <= 5; i++) {
+        var mark = Number(GetInput("Mark " + i));
+        sum += mark;
+    }
+    var totalMarks = "Total marks: " + sum;
+    var averageMarks = "Average marks: " + (sum / 5);
+    var percentage = "Percentage: " + (sum / (5 * 100)) * 100;
+    displayResult("Total marks: "+sum)
+    displayResult("Average marks: "+sum/5)
+        //it will be the same as the above one as the max mark=100
+    displayResult("percentage: "+(sum/(5*100))*100)
+        }
+
+//question13
+function daysInMonth() {
+    var monthNumber = GetInput("Month number");
+     $30DaysMonths = [4, 6, 9, 11];
+    let daysInMonth;
+    if (monthNumber == 2) {
+        daysInMonth = 28;
+    } else if ($30DaysMonths.includes(parseInt(monthNumber))) {
+        daysInMonth = 30;
+    } else {
+        daysInMonth = 31;
+    }
+    displayResult("Days in Month: " + daysInMonth);
+}
 
 
-// function question8() {
-//     var character = GetInput("a character");
-//     if (isVowel(character)) {
-//         displayInAlert("Vowel")
-//     } else {
-//         displayInAlert("Consonant");
-//     }
-// }
+//question14
+function grade() {
+    var sum=0
+    for (var i = 0; i < 5; i++) {
+        var mark =Number( GetInput("mark " + (i + 1)));
+        sum+=Number(mark)
+        var grade = getGrade(mark);
+        displayInAlert(grade);
+    }
+}
 
+//question15
+function MonthSecondMethod() {
+    var monthNumber = Number(GetInput("Month number"));
+    $30DaysMonths = [4, 6, 9, 11];
+    let daysInMonth;
 
-// function question9() {
-//     var number = GetInput("a number");
-//     var numbersPrinted = "";
-//     for (let i = 1; i <= number; i++) {
-//         numbersPrinted += i + " ";
-//     }
-//     displayResult(numbersPrinted)
-// }
+    switch (monthNumber) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            daysInMonth = 30;
+            break;
+        case 2:
+            daysInMonth = 28;
+            break;
+        default:
+            daysInMonth = 31;
+    }
 
-// function question10() {
-//     var number = GetInput("a number");
-//     let numbersPrinted = "";
-//     for (let i = 1; i <= 12; i++) {
-//         numbersPrinted += number * i + " ";
-//     }
-//     displayResult(numbersPrinted);
-// }
+    displayResult("Days in Month: " + daysInMonth);
+}
+//question16
+function isVowelSecondMethod() {
+    var character = GetInput("a character");
+    switch(character){
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            displayInAlert("Vowel")
+            break
+        default:
+            displayInAlert("constonant")
 
-// function question11() {
-//     var number = GetInput("a number");
-//     let numbersPrinted = "";
-//     for (let i = 1; i <= number; i++) {
-//         if (i % 2 == 0) {
-//             numbersPrinted += i + " ";
-//         }
-//     }
-//     displayResult(numbersPrinted);
-// }
+    }
+}
+//question17
+function maxNumberSecondMethod() {
+    var number1 = GetInput("number 1");
+    var number2 = GetInput("number 2");
+    var subtract = number1 - number2;
+    switch(true){
+        case subtract>0:
+            displayResult("Max number is " + number1)
+            break
+        case subtract<0:
+            displayResult("Max number is " + number2);
+            break
+        default:
+            displayResult("Max number is " + number1);
+    }   
+}
 
-// function question12() {
-//     var number = GetInput("a number");
-//     var power = GetInput("the power");
-//     displayInAlert(Math.pow(number, power));
-// }
-
-// function question12Second() {
-//     var sum = 0;
-//     for (let i = 1; i <= 5; i++) {
-//         var mark = Number(GetInput("Mark " + i));
-//         sum += mark;
-//     }
-//     var totalMarks = "Total marks: " + sum;
-//     var averageMarks = "Average marks: " + (sum / 5);
-//     var percentage = "Percentage: " + (sum / (5 * 100)) * 100;
-//     displayResult("Total marks: "+sum)
-//     displayResult("Average marks: "+sum/5)
-//         //it will be the same as the above one as the max mark=100
-//     displayResult("percentage: "+(sum/(5*100))*100)
-//         }
-
-// function question13() {
-//     var monthNumber = GetInput("Month number");
-//      $30DaysMonths = [4, 6, 9, 11];
-//     let daysInMonth;
-
-//     if (monthNumber == 2) {
-//         daysInMonth = 28;
-//     } else if ($30DaysMonths.includes(parseInt(monthNumber))) {
-//         daysInMonth = 30;
-//     } else {
-//         daysInMonth = 31;
-//     }
-//     displayResult("Days in Month: " + daysInMonth);
-// }
-
-// function getGrade(mark) {
-//     var percentage = (mark / 100) * 100; // Assuming full mark is 100
-//     if (percentage >= 90) {
-//         return "Grade A";
-//     } else if (percentage >= 80) {
-//         return "Grade B";
-//     } else if (percentage >= 70) {
-//         return "Grade C";
-//     } else if (percentage >= 60) {
-//         return "Grade D";
-//     } else if (percentage >= 40) {
-//         return "Grade E";
-//     } else {
-//         return "Grade F";
-//     }
-// }
-// function question14() {
-//     for (let i = 0; i < 5; i++) {
-//         var mark = GetInput("mark " + (i + 1));
-//         var grade = getGrade(mark);
-//         displayInAlert(grade);
-//     }
-// }
-
-// function question15() {
-//     var monthNumber = Number(GetInput("Month number"));
-//     $30DaysMonths = [4, 6, 9, 11];
-//     let daysInMonth;
-
-//     switch (monthNumber) {
-//         case 4:
-//             daysInMonth = 30;
-//             break;
-//         case 6:
-//             daysInMonth = 30;
-//              break;
-//         case 9:
-//             daysInMonth = 30;
-//             break;
-//         case 11:
-//             daysInMonth = 30;
-//             break;
-//         case 2:
-//             daysInMonth = 28;
-//             break;
-//         default:
-//             daysInMonth = 31;
-//     }
-
-//     displayResult("Days in Month: " + daysInMonth);
-// }
-
-// function question16() {
-//     var character = GetInput("a character");
-//     switch(character){
-//         case "a":
-//             displayInAlert("Vowel")
-//             break
-//         case "e":
-//             displayInAlert("Vowel")
-//             break
-//         case "i":
-//             displayInAlert("Vowel")
-//             break
-//         case "o":
-//             displayInAlert("Vowel")
-//             break
-//         case "u":
-//             displayInAlert("Vowel")
-//             break
-//         default:
-//             displayInAlert("constonant")
-
-//     }
-// }
-
-// function question17() {
-//     var number1 = GetInput("number 1");
-//     var number2 = GetInput("number 2");
-//     var subtract = number1 - number2;
-//     switch(true){
-//         case subtract>0:
-//             displayResult("Max number is " + number1)
-//             break
-//         case subtract<0:
-//             displayResult("Max number is " + number2);
-//             break
-//         default:
-//             displayResult("Max number is " + number1);
-//     }   
-// }
- 
-// function question18() {
-//     var number = GetInput("a number");
-//     switch(number%2==0){
-//             case true:
-//                 window.alert("Even")
-//                 break
-//             case false:
-//                 window.alert("Odd")
-//                 break
-//         }
-// }
-
-// function question19() {
-//     var number = GetInput("a number");
-//     if (number > 0) {
-//         window.alert("Positive");
-//     } else if (number < 0) {
-//         window.alert("Negative");
-//     } else {
-//         window.alert("Zero");
-//     }
-// }
-
-function Question20(){
+//question18
+function evenOrOdddSecondMetthod() {
+    var number = GetInput("a number");
+    switch(number%2==0){
+            case true:
+                window.alert("Even")
+                break
+            case false:
+                window.alert("Odd")
+                break
+        }
+}
+//question19
+function PositiveOrNegativeSecondMethod() {
+    var number = GetInput("a number");
+    if (number > 0) {
+        window.alert("Positive");
+    } else if (number < 0) {
+        window.alert("Negative");
+    } else {
+        window.alert("Zero");
+    }
+}
+//Question 20
+function Operation(){
 var number1 =(GetInput("number 1 "))
 var number2 =(GetInput("number 2 "))
 var operation=GetInput("Enter the operation (+,-,*,/)")
@@ -292,7 +295,6 @@ switch(operation){
 
 
 
-
 function addText(id,counter,text){
     var currentDiv=document.getElementById(id)
     var senetce =""
@@ -301,16 +303,15 @@ function addText(id,counter,text){
     }
     currentDiv.innerHTML=senetce
 }
+
 addText("firstdiv",10,"ITI")
 addText("seconddiv",15,"Web ")
 addText("thirddiv",20,"FCDS")
 addText("fourthdiv",25,"ITI")
 addText("fifthdiv",30,"Web ")
 
-function addImage(id,path){
-    var currentDiv=document.getElementById(id).innerHTML="<img src=images/ITI.png>"
-    
+function addImage(id,alt){
+    document.getElementById(id).innerHTML="<img src=images/ITI.png alt="+alt+">"
 }
-addImage("image",123)
+addImage("image","ITI")
 
-Question20()
